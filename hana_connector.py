f6538d2cc56a72e3bf89b7abba626ab74ef6c06d
mockingbird2 <fabian.paul1@gmx.net>
2015-04-29 10:41:53 +0200

    fixed

--- hana_connector.py
+++ hana_connector.py
@@ -1,9 +1,9 @@
 import pyhdb
 
 connection = pyhdb.connect(
-	host="192.168.30.206", 
+	host="", 
 	port=30015, 
-	user="SYSTEM", 
-	password="manager")
+	user="", 
+	password="")
 
 connection.close()
