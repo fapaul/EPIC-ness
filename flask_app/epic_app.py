eaae3d35e7a823f17e490040a3220cb56288e6e6
mockingbird2 <fabian.paul1@gmx.net>
2015-05-08 18:32:41 +0200

    add flask framework and setup ork and setup first templates

---
+++ flask_app/epic_app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from credentials_hana import db_HOST, db_PORT, db_USER, db_PASSWORD, flask_user, flask_password
+from secret_key import SECRET
+import pyhdb
+
+#configuration
+DEBUG = True
+SECRET_KEY = SECRET
+USERNAME = flask_user
+PASSWORD = flask_password
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db(): 
+	return pyhdb.connect(
+		host = db_HOST,
+		port = db_PORT,
+		user = db_USER,
+		password = db_PASSWORD)
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+@app.route('/')
+def show_entries():
+	print "app route"
+	cur = g.db.cursor()
+	print cur
+	print "cursor initialized"
+	cur.execute("SELECT DRIVER FROM NYCCAB.FARE")
+	print "Query succeed"
+	entries = cur.fetchone()
+	return render_template('show_entries.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+	app.run()
+
--- hana_connector.py
+++ hana_connector.py
@@ -7,7 +7,4 @@ connection = pyhdb.connect(
 	user=USER, 
 	password=PASSWORD)
 
-cursor = connection.cursor()
-cursor.execute("SELECT Name FROM FARE")
-print(cursor.fetchall())
 connection.close()
