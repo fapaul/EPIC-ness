5f767431f56cedff32b7198287e6dc005613f713
weaslbe <ajay.kesar@student.hpi.de>
2015-06-01 22:18:46 +0200

    added HeatMap with Test Data
    shows the pickup points for the 100 highest rates

---
+++ flask_app/static/indexOld.js
@@ -0,0 +1,36 @@
+var map;
+
+
+$(function getMarker(){
+	$.getJSON($SCRIPT_ROOT + "/showMarker", function(data){
+		console.log(data[0]);
+		initialize(data[0]);
+		google.maps.event.addDomListener(window, 'load', initialize);
+		for (point in data){
+			buildMarker(data[point])
+		}
+	});
+});
+
+function initialize(point) {
+	var latLong = new google.maps.LatLng(point.lat, point.long)
+	var mapOptions = {
+		zoom: 15,
+		center: latLong
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+	console.log("map initialized");
+
+}
+
+function buildMarker(point){
+	console.log('building marker')
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(point.lat, point.long),
+		map: map,
+		title: 'test'
+	});
+}
+
+
